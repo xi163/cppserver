2a789e64536ed5c3d26a9cb9a3fe3b5c47d7a2fc
ro <luochengwu888@gmail.com>
2023-08-15 20:39:41 +0800

    commit

--- Framework/game_api/rpc/server/RpcService.cc
+++ Framework/game_api/rpc/server/RpcService.cc
@@ -19,7 +19,7 @@ namespace rpc {
 				rsp.set_domain(gServer->proto_ + gServer->server_.ipPort() + gServer->path_handshake_);
 				break;
 			case 1:
-				rsp.set_host(gServer->http_proto_ + gServer->httpserver_.ipPort() + gServer->path_http_order_);
+				rsp.set_host(gServer->http_proto_ + gServer->httpserver_.ipPort()/* + gServer->path_http_order_*/);
 				rsp.set_domain(gServer->http_proto_ + gServer->httpserver_.ipPort() + gServer->path_http_order_);
 			}
 			//_LOG_WARN("\nreq:%s\nrsp:%s", req->DebugString().c_str(), rsp.DebugString().c_str());
