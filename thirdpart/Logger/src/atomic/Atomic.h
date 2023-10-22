7a9982bb2be020509b858331fc1bed248d21ad54
ro <luochengwu888@gmail.com>
2023-10-22 21:49:26 +0800

    commit

--- thirdpart/Logger/src/atomic/Atomic.h
+++ thirdpart/Logger/src/atomic/Atomic.h
@@ -4,84 +4,83 @@
 #include "Logger/src/Macro.h"
 
 #ifdef _windows_
-#define atom_incr(i)			InterlockedIncrement((volatile long *)&i)
-#define atom_incr64(i)			InterlockedIncrement64((volatile long long *)&i)
-#define atom_decr(i)			InterlockedDecrement((volatile long *)&i)
-#define atom_decr64(i)			InterlockedDecrement64((volatile long long *)&i) // return new value
-#define atom_add(i,val)			InterlockedExchangeAdd((volatile long *)&i,(long)(val))
-#define atom_add64(i,val)		InterlockedExchangeAdd64((volatile long long *)&i,(long long)(val))
-#define atom_sub(i,val)			InterlockedExchangeAdd((volatile long *)&i,-(long)(val))
-#define atom_sub64(i,val)		InterlockedExchangeAdd64((volatile long long *)&i,-(long long)(val))
-#define atom_assign(i, val)		InterlockedExchange((volatile long *)&i, (long)(val))
-#define atom_assign64(i,val)	InterlockedExchange64((volatile long long *)&i, (long long)(val)) // return old value
-#define atom_compare_assign(i,val,cmp) InterlockedCompareExchange((volatile long *)&i, (long)(val), (long)(cmp))
-#define atom_compare_assign64(i,val,cmp) InterlockedCompareExchange64((volatile long long *)&i, (long long)(val), (long long)(cmp))
-#define atom_assign_pointer(i, val) InterlockedExchangePointer(( void * volatile *)&i, (void *)(val))
-#define atom_compare_assign_pointer(i,val,cmp) InterlockedCompareExchangePointer((void * volatile *)&i, (void *)(val), (void *)(cmp))
+
+#define incrementAndGet(dst)		InterlockedIncrement((long *)&dst)
+#define decrementAndGet(dst)		InterlockedDecrement((long *)&dst)
+
+#define addAndGet(dst, val)			InterlockedExchangeAdd((long *)&dst, (long)(val))
+#define subAndGet(dst, val)			InterlockedExchangeAdd((long *)&dst, -(long)(val))
+
+#define getAndSet(dst, val)		    InterlockedExchange((long *)&dst, (long)(val))
+#define getAndSetPtr(dst, val)		InterlockedExchangePointer((void **)&dst, (void *)(val))
+
+#define getCmpSet(dst, cmp, val)    InterlockedCompareExchange((long *)&dst, (long)(val), (long)(cmp))
+#define getCmpSetPtr(dst, cmp, val) InterlockedCompareExchangePointer((void **)&dst, (void *)(val), (void *)(cmp))
+
 #elif defined(_linux_)
-#define atom_incr(i)			__sync_add_and_fetch(&i, 1)
-#define atom_incr64(i)			__sync_add_and_fetch(&i, 1)
-#define atom_decr(i)			__sync_add_and_fetch(&i, -1)
-#define atom_decr64(i)			__sync_add_and_fetch(&i, -1)
-#define atom_add(i,val)			__sync_add_and_fetch(&i, (val))
-#define atom_add64(i,val)		__sync_add_and_fetch(&i, (val))
-#define atom_sub(i,val)			__sync_add_and_fetch(&i, -(val))
-#define atom_sub64(i,val)		__sync_add_and_fetch(&i, -(val))
-#define atom_assign(i, val)		__sync_lock_test_and_set(&i, (val)) // __sync_val_compare_and_swap(&i, i, (val)) __sync_lock_release
-#define atom_assign64(i,val)	__sync_lock_test_and_set(&i, (val)) // __sync_val_compare_and_swap(&i, i, (val)) __sync_lock_release
-//#define atom_compare_assign(i,val,cmp) __sync_val_compare_and_swap(&i, cmp, (val))
-//#define atom_compare_assign64(i,val,cmp) __sync_val_compare_and_swap(&i, cmp, (val))
-//#define atom_assign_pointer(i, val) __sync_lock_test_and_set(&i, (val))
-//#define atom_compare_assign_pointer(i,val,cmp) __sync_val_compare_and_swap(&i, cmp, (val))
+
+#define getAndIncrement(dst)	    __sync_fetch_and_add(&dst, 1)
+#define getAndDecrement(dst)	    __sync_fetch_and_add(&dst, -1)
+
+#define getAndAdd(dst,val)		    __sync_fetch_and_add(&dst, val)
+#define getAndSub(dst,val)		    __sync_fetch_and_add(&dst, -(val))
+
+#define incrementAndGet(dst)	    __sync_add_and_fetch(&dst, 1)
+#define decrementAndGet(dst)	    __sync_add_and_fetch(&dst, -1)
+
+#define addAndGet(dst,val)		    __sync_add_and_fetch(&dst, val)
+#define subAndGet(dst,val)		    __sync_add_and_fetch(&dst, -(val))
+
+#define getAndSet(dst, val)		    __sync_lock_test_and_set(&dst, (val))
+#define getAndSetPtr(dst, val)      __sync_lock_test_and_set(&dst, (val))
+
+#define getCmpSet(dst, cmp, val)    __sync_val_compare_and_swap(&dst, cmp, (val))
+#define getCmpSetPtr(dst, cmp, val) __sync_val_compare_and_swap(&dst, cmp, (val))
+
 #endif
+
+#define CAS(ptr, oldval, newval) getCmpSetPtr(ptr, oldval, newval) == (oldval)
 
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\//\/\/\/
 /* 原子锁 */
-// class Atom
-// {
-// 	/* 空闲/占有 */
-// 	enum _{ kIdle_ = 0, kLocked_ = 1, };
+// class AtomicLock {
+// 	enum _ { kIdle_ = 0, kLocked_ = 1, };
 // public:
-// 	class GuardLock
-// 	{
+// 	class GuardLock {
 // 	public:
-// 		GuardLock(Atom& lock) :lock_(lock) {
+// 		GuardLock(AtomicLock& lock) :lock_(lock) {
 // 			lock_.lock();
 // 		}
 // 		~GuardLock() {
 // 			lock_.unlock();
 // 		};
 // 	private:
-// 		Atom& lock_;
+// 		AtomicLock& lock_;
 // 	};
 // public:
-// 	Atom() :atom_(kIdle_) {  }
-// 	/* 原子加锁 */
+// 	AtomicLock() : val_(kIdle_) {  }
 // 	inline void lock() {
-// 		do{
+// 		do {
 // 			xsleep(0);
 // 		} while (!tryLock());
 // 	}
-// 	/* 释放锁 */
 // 	inline void unlock() {
 // 		do {
 // 			tryUnLock();
 // 		} while (0);
 // 	}
 // private:
-// 		inline bool tryLock()
-// 		{
-// 			/* 如果之前空闲, 尝试抢占, 并返回之前状态
-// 			 * 如果返回空闲, 说明刚好抢占成功, 否则说明之前已被占有 */
-// 			 return kIdle_ == atom_compare_assign(atom_, kLocked_, kIdle_);
-// 		}
-// 		inline bool tryUnLock()
-// 		{
-// 			/* 谁占有谁释放 */
-// 			return kLocked_ == atom_compare_assign(atom_, kIdle_, kLocked_);
-// 		}
+// 	inline bool tryLock() {
+// 		/* 如果之前空闲, 尝试抢占, 并返回之前状态
+// 		 * 如果返回空闲, 说明刚好抢占成功, 否则说明之前已被占有 */
+// 		return kIdle_ == getCmpSet(val_, kIdle_, kLocked_);
+// 	}
+// 	inline bool tryUnLock() {
+// 		/* 谁占有谁释放 */
+// 		return kLocked_ == getCmpSet(val_, kLocked_, kIdle_);
+// 	}
 // private:
-// 	volatile long atom_;
+// 	volatile long val_;
 // };
 
 #endif
